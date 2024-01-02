08dc2d0e759bb986a8381974b78ba0014db6d8a6
oluwatobimaxwell <tobisholanke@gmail.com>
2024-01-02 09:46:53 +0000

    added firebase

--- webcontent/config/middlewares.js
+++ webcontent/config/middlewares.js
@@ -8,12 +8,11 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           'media-src': [
             "'self'",
             'data:',
             'blob:',
-            'market-assets.strapi.io',
             'res.cloudinary.com',
           ],
           upgradeInsecureRequests: null,
