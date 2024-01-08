b0f401b258265a3f9dade1da1230c4527ece1b1c
oluwatobimaxwell <tobisholanke@gmail.com>
2024-01-08 01:07:42 +0000

    update

--- webnew/src/api/clients.ts
+++ webnew/src/api/clients.ts
@@ -1,4 +1,10 @@
 import { AboutMeDataType, AcademicDataType, MainDataType, PublicationType, WelcomeDataType } from "./types";
+import mainIntroImage from "../assets/web/image/img6.jpg";
+import aboutImage from "../assets/web/image/img3.jpg";
+import knustIcon from "../assets/web/image/schools/knustlogo.png";
+import teriIcon from "../assets/web/image/schools/TERI-school-logo.png";
+import carnegiIcon from "../assets/web/image/schools/logo_solid.png";
+import academicImage from "../assets/web/image/img5.jpg";
 
 // Define the URL for the API endpoint
 const apiHost = import.meta.env.VITE_API_BASE_URL;
@@ -13,15 +19,18 @@ export async function getMainData(): Promise<MainDataType> {
             return response.json();
         }).then((res) => {
             const attributes = res.data.attributes;
-            const introImage = `${apiHost}${attributes.introImage.data.attributes.formats.large.url}`;
-            const schoolIcons = (attributes.schoolIcons.data || []).map((img: any) => ({
-                name: img?.attributes.name,
-                url: `${apiHost}${img.attributes.formats.medium.url}`
-            }));
+            // const resImage = `${apiHost}${attributes.introImage.data.attributes.formats.large.url}`;
+            // const schoolIcons = (attributes.schoolIcons.data || []).map((img: any) => ({
+            //     name: img?.attributes.name,
+            //     url: `${apiHost}${img.attributes.formats.medium.url}`
+            // }));
             return {
                 ...attributes,
-                introImage,
-                schoolIcons,
+                introImage: mainIntroImage,
+                schoolIcons: [knustIcon, teriIcon, carnegiIcon].map((url) => ({
+                    name: "School Icon", 
+                    url,
+                })),
              }
         })
 }
@@ -48,10 +57,10 @@ export async function getAboutMeData(): Promise<AboutMeDataType> {
             return response.json();
         }).then((res) => {
             const attributes = res.data.attributes;
-
+            // const resImage = `${apiHost}${attributes.image.data.attributes.formats.large.url}`;
             return {
                 ...attributes,
-                image: `${apiHost}${attributes.image.data.attributes.formats.large.url}`,
+                image: aboutImage,
              }
         })
 }
@@ -79,7 +88,8 @@ export async function getAcademicData(): Promise<AcademicDataType> {
             const attributes = res.data.attributes;
             return {
                 ...attributes,
-                image: `${apiHost}${attributes.image.data.attributes.formats.large.url}`,
+                image: academicImage, 
+                //`${apiHost}${attributes.image.data.attributes.formats.large.url}`,
              }
         })
 }
